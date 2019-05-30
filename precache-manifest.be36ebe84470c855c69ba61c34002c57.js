69d281fb330fc6f5566ec231ceeff5426bb35502
Yash Mishra <yash.mishra@raweng.com>
2019-05-30 18:13:21 +0530

    Updates

---
+++ precache-manifest.be36ebe84470c855c69ba61c34002c57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005798ca07bad270ee069e71a7f2fd38",
+    "url": "/coin-flip-react/index.html"
+  },
+  {
+    "revision": "f2b27246a2ffcf24e964",
+    "url": "/coin-flip-react/static/css/main.83545720.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/coin-flip-react/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "f2b27246a2ffcf24e964",
+    "url": "/coin-flip-react/static/js/main.ec1fea19.chunk.js"
+  },
+  {
+    "revision": "5023c423d74bc26dab8b",
+    "url": "/coin-flip-react/static/js/runtime~main.3edfad78.js"
+  }
+]);
